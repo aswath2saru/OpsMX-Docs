80996bc8ed5a38597ef4d2b8830267dc2f1c0cbc
Saravanan Arumugam <41325803+aswath2saru@users.noreply.github.com>
2020-04-27 21:26:14 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
     'recommonmark',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -79,8 +80,8 @@ pygments_style = None
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-#html_theme = ' '
-RTD_NEW_THEME = True
+html_theme = "sphinx_rtd_theme"
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
